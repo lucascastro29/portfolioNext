9e8038e3bd3129185e8ed0ff0b1449c37f6ca9e7
Lucas Castro <lucascastro2929@gmail.com>
2023-01-02 19:14:28 -0300

    a

--- firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "AIzaSyAPrq1BCFrAhfSSW7EMqJRmGJGWqs2tce4",
-  authDomain: "portfoliolucascastro.firebaseapp.com",
-  projectId: "portfoliolucascastro",
-  storageBucket: "portfoliolucascastro.appspot.com",
-  messagingSenderId: "303913010055",
-  appId: "1:303913010055:web:d7591122b0eb5a22624922",
-};
