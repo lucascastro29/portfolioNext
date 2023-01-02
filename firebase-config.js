94beee25c2c488ffc01add716a1bd7a6070d7db8
Lucas Castro <lucascastro2929@gmail.com>
2023-01-02 15:15:41 -0300

    kill me

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBBUFFmHCsc9LxD8Bwz2sq3XL2_AvIzJX0",
-  authDomain: "portfolio-351117.firebaseapp.com",
-  projectId: "portfolio-351117",
-  storageBucket: "portfolio-351117.appspot.com",
-  messagingSenderId: "466503299432",
-  appId: "1:466503299432:web:df78f9b1bd899c401b26dd",
-  measurementId: "G-BJEHLEY452",
-};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,13 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import {EcommerceProvider} from '../components/context/PortfolioContext';
 import Head from 'next/head';
-import { FirebaseAppProvider } from 'reactfire';
-import firebaseConfig from "../firebase-config"
 function MyApp({ Component, pageProps }) {
 
   
   return (
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <EcommerceProvider>
         <Component {...pageProps} />
       </EcommerceProvider>
-    </FirebaseAppProvider>
   );
 }
 
